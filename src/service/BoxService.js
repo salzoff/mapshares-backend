dba68a0fd5d3b28561933e3fb3347f247b06a307
salzoff <salzoff@gmail.com>
2019-04-19 07:19:18 +0200

    latest

--- src/service/BoxService.js
+++ src/service/BoxService.js
@@ -1,6 +1,8 @@
+import admin from 'firebase-admin';
 import { boxCollection, geoCollection, firestore } from '../../firebaseConfig.js';
 import { geoDistance } from '../helper/location';
 import config from '../config';
+import { deepStrictEqual } from 'assert';
 let instance = false;
 
 export default class BoxService {
@@ -11,9 +13,53 @@ export default class BoxService {
         instance = this;
     }
 
-    getBoxesForLocation(latitude, longitude) {
+    getBoxesForLocation(user, latitude, longitude) {
         console.log(firestore.collection('box'));
         console.log(boxCollection);
+        const userLocation = {
+            lat: latitude,
+            lng: longitude
+        };
+        geoCollection.near({
+            center: new admin.firestore.GeoPoint(latitude, longitude),
+            radius: config.boxQueryDistance
+        }).get().then(result => {
+            responseObj = {};
+            result.forEach(entry => {
+                const entryData = entry.data();
+                switch (entryData.objectType) {
+                    case 1:
+                        if (user.permissions.includes('SHOW_USER_LOCATION')) {
+                            responseObj.user.push(entryData);
+                        }
+                        break;
+                    case 2:
+                        if (user.permissions.includes('SHOW_BOX_LOCATION')) {
+                            responseObj.box.push(entryData);
+                        } else {
+                            const distance = geoDistance(userLocation, {
+                                lat: entryData.latitude,
+                                lng: entryData.longitude
+                            });
+                            if (distance <= config.boxLocationMaxDistance) {
+                                responseObj.box.push({
+                                    position: entryData.position,
+                                    id: entry.id,
+                                    title: entryData.title,
+                                    value: entryData.value
+                                })
+                            } else {
+                                responseObj.boxNearby.push({
+                                    id: entry.id,
+                                    title: entryData.title,
+                                    distance: distance
+                                });
+                            }
+                        }
+                    case 3:
+                }
+            })
+        });
         return boxCollection.doc('PqcQav50relKB12KarGL').get().then(result => {
             return result;
         });
